b53dbcfdbaad85c3b1c9f00f835bb499d26c71a7
AMALENDU MANOJ <220229@tkmce.ac.in>
2024-03-22 18:40:26 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Event from './components/Event';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App ">
       <Event />
+      <Contact />
     </div>
   );
 }
--- src/components/Event.js
+++ src/components/Event.js
@@ -3,57 +3,61 @@ import { y2024, y2023, y2022 } from '../data/eventlist'
 import Eventcard from './Eventcard'
 const Event = () => {
     return (
-        <div className='p-10'>
+        <div className='p-10 bg-sky-700'>
             <div className='flex justify-center '>
                 <h1 className='text-6xl'>EVENTS</h1>
             </div>
+            <div className='flex justify-center items-center'>
+                <div className='grid  lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 justify-center items-center pt-10 space-y-10 space-x-5'>
 
-            <div className='grid grid-cols-4 justify-center items-center pt-10 space-y-10'>
-                <div className='bg-blue-700 w-10 h-fit p-2'>
-                    <div className='flex flex-col' >
-                        <p>2</p>
-                        <p>0</p>
-                        <p>2</p>
-                        <p>4</p>
+                    <div className='bg-blue-700 sm:w-10 sm:h-fit w-48 h-10 p-2 md:ml-0 ml-5 '>
+                        <div className='flex sm:flex-col justify-center items-center text-center' >
+                            <p>2</p>
+                            <p>0</p>
+                            <p>2</p>
+                            <p>4</p>
+                        </div>
                     </div>
-                </div>
-                {
-                    y2024.map((event) => {
-                        return (
-                            <Eventcard event={event} />
-                        )
-                    })
-                }
-                <div className='bg-blue-700 w-10 h-fit p-2'>
-                    <div className='flex flex-col' >
-                        <p>2</p>
-                        <p>0</p>
-                        <p>2</p>
-                        <p>3</p>
+
+                    {
+                        y2024.map((event) => {
+                            return (
+                                <Eventcard event={event} />
+                            )
+                        })
+                    }
+                    
+                    <div className='bg-blue-700 sm:w-10 sm:h-fit w-48 h-10 p-2'>
+                        <div className='flex sm:flex-col justify-center items-center text-center' >
+                            <p>2</p>
+                            <p>0</p>
+                            <p>2</p>
+                            <p>3</p>
+                        </div>
                     </div>
-                </div>
-                {
-                    y2023.map((event) => {
-                        return (
-                            <Eventcard event={event} />
-                        )
-                    })
-                }
-                <div className='bg-blue-700 w-10 h-fit p-2'>
-                    <div className='flex flex-col' >
-                        <p>2</p>
-                        <p>0</p>
-                        <p>2</p>
-                        <p>2</p>
+                    {
+                        y2023.map((event) => {
+                            return (
+                                <Eventcard event={event} />
+                            )
+                        })
+                    }
+                    <div className='bg-blue-700 sm:w-10 sm:h-fit w-48 h-10 p-2'>
+                        <div className='flex sm:flex-col justify-center items-center text-center' >
+                            <p>2</p>
+                            <p>0</p>
+                            <p>2</p>
+                            <p>2</p>
+                        </div>
                     </div>
+                    {
+                        y2022.map((event) => {
+                            return (
+                                <Eventcard event={event} />
+                            )
+                        })
+                    }
                 </div>
-                {
-                    y2022.map((event) => {
-                        return (
-                            <Eventcard event={event} />
-                        )
-                    })
-                }
             </div>
         </div>
     )
