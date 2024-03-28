8e2872b980674c17bcf4da5cdd876fb2f497165c
DEVANAND.S <133562029+Devanand999@users.noreply.github.com>
2024-03-28 23:31:34 +0530

    changed

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -17,21 +17,21 @@ const Footer = () => {
          <div className="footer-right">
           <div className="footer-links-div-right">
             <h4>ABOUT</h4>
-            <a href="/employer">
+      
               <p>about</p> 
-            </a>
+            
         </div>
         <div className="footer-links-div-right">
             <h4>TEAM</h4>
-            <a href="/employer">
+            
               <p>team</p> 
-            </a>
+            
         </div>
         <div className="footer-links-div-right">
             <h4>CONTACT</h4>
-            <a href="/employer">
+            
               <p>contact</p> 
-            </a>
+            
         </div>
         </div>
 
