51d4e4ecdf4384d4d7e20953ea69f428aa747489
AMALENDU MANOJ <220229@tkmce.ac.in>
2024-03-29 13:15:48 +0530

    Update Gallery.js

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -7,7 +7,7 @@ const Events = () => {
 
   return (
     <div className='flex flex-col items-center justify-center pb-10 bg-white'  >
-      <div className="font-Poppins font-extrabold text-4xl text-center backdrop text-white pt-28" ><h1>Gallery</h1></div>
+      <div className="font-Poppins font-extrabold text-4xl text-center backdrop text-[#0B0B51] font-poppins pt-28" ><h1>Gallery</h1></div>
       <div className='mt-12 flex flex-wrap justify-center items-center gap-5 pr-10 pl-10'>
       {
         y2024.map((event) => {
